ba507822d5149e9f4128a3e02813e0903794dc7e
Pavan Peshve <pavanpeshve99@gmail.com>
2019-10-14 21:41:15 +0530

    sgreg

---
+++ sfgerg.java
@@ -0,0 +1,37 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import pom.Unpage;
+import pom.registerpage;
+public class Mainprogram{
+
+	public static void main(String[] args) 
+	{
+	   System.setProperty("webdriver.chrome.driver", "E:\\SELENI\\chromedriver.exe");
+	   WebDriver driver=new ChromeDriver();
+	   driver.get("https://www.zoopla.co.uk");
+	   
+	   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	   driver.manage().window().maximize();
+	   driver.findElement(By.xpath("//button [.='Accept']")).click();
+	   
+	   Unpage up=new Unpage(driver);
+	   up.clickReg_link();
+	   up.clickReg_link();
+	   
+	   registerpage rp=new registerpage(driver);
+	   rp.setEmail("pavanpeshve99@gmail.com");
+	   rp.setPwd("samurai89");
+	   rp.clickDrp_dn();
+	   rp.slct_option();
+	   rp.regbtn_click();
+	   
+	}
+
+}
